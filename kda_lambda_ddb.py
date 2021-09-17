697fc8b74c68da9520966d0a2d58742fbb1f59c7
agoswami0915 <38989589+agoswami0915@users.noreply.github.com>
2021-09-16 20:06:22 -0700

    Update kda_lambda_ddb.py

--- kda_lambda_ddb.py
+++ kda_lambda_ddb.py
@@ -9,7 +9,7 @@
 # and the output record format type is specified as JSON.
 #
 # A sample output record from Kinesis Analytics application for this function is as below
-# {    "RFID": 6989139522658,    "TollID: 179,    "Date-Month": "JAN",    "Date-Day": 27, "Date-Year": 2019,   "Vehicle_Type: "Car"}
+# {    "RFID": 6989139522658,    "TollID": 179,    "Date-Month": "JAN",    "Date-Day": 27, "Date-Year": 2019,   "Vehicle_Type": "Car"}
 from __future__ import print_function
 import boto3
 import base64
